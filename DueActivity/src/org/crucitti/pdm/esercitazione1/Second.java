9b4bad7b7720838a092d80bc4ce373896b214d1e
Gianluca Crucitti <gianluca1988@hotmail.it>
2012-01-18 13:29:45 +0100

    DueActivity

---
+++ DueActivity/src/org/crucitti/pdm/esercitazione1/Second.java
@@ -0,0 +1,15 @@
+package org.crucitti.pdm.esercitazione1;
+import org.crucitti.pdm.esercitazione1.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Second extends Activity{
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+	
+}
+
